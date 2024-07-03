c5f658b43ec2747e4c43cbb1aabc65034e505250
Edmund Ochieng <ochienged@gmail.com>
2024-07-03 13:01:04 -0500

    test for the creation of managed content
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

--- internal/contentmanager/contentmanager_test.go
+++ internal/contentmanager/contentmanager_test.go
@@ -1,18 +1,74 @@
 package contentmanager
 
 import (
+	"context"
 	"testing"
 
+	"github.com/operator-framework/operator-controller/api/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-func TestManageContent(t *testing.T) {}
+func TestManageContent(t *testing.T) {
+	tests := []struct {
+		name    string
+		rcm     RestConfigMapper
+		config  *rest.Config
+		ce      *v1alpha1.ClusterExtension
+		objs    []client.Object
+		wantErr bool
+	}{
+		{
+			name: "Valid cluster extension valid managed content should pass",
+			rcm: func(_ context.Context, _ client.Object, cfg *rest.Config) (*rest.Config, error) {
+				return cfg, nil
+			},
+			config: &rest.Config{},
+			ce: &v1alpha1.ClusterExtension{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-cluster-extension",
+				},
+			},
+			objs: []client.Object{
+				&corev1.Pod{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "v1",
+						Kind:       "Pod",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "webserver",
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			mgr, _ := manager.New(tc.config, manager.Options{})
+			ctrl, err := controller.New("test-controller", mgr, controller.Options{
+				Reconciler: reconcile.Func(func(context.Context, reconcile.Request) (reconcile.Result, error) {
+					return reconcile.Result{}, nil
+				}),
+			})
+			require.NoError(t, err)
+
+			instance := New(tc.rcm, tc.config, mgr.GetRESTMapper())
+			got := instance.ManageContent(context.Background(), ctrl, tc.ce, tc.objs)
+			assert.Equal(t, got != nil, tc.wantErr)
+		})
+	}
+}
 
 func TestBuildScheme(t *testing.T) {
 	type validation struct {
